62a530228b1709ab876cb323d1569290bb0368db
Nicolas Bouliane <contact@nicolasbouliane.com>
2022-04-05 16:24:33 +0200

    usage examples

--- dotfiles/functions.sh
+++ dotfiles/functions.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
+
+PATH_TO_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+# Usage:
+# `alert "Hello world"`
 function alert {
     osascript -e 'display notification "'"$1"'"'
 }
 
+# Usage:
+# `proxy on`
+# `proxy off`
 function proxy {
     is_proxy_on=$(networksetup -getsocksfirewallproxy wi-fi | grep "No")
     if [ -n "$is_proxy_on" ] | [ "$1" == "on" ]; then
@@ -13,4 +21,10 @@ function proxy {
         echo "Turning proxy off"
         sudo networksetup -setsocksfirewallproxystate wi-fi off
     fi
+}
+
+# Usage:
+# `project HomeServer`
+function project {
+    python3 "$PATH_TO_SCRIPT_DIR/../scripts/project.py" "$@"
 }
