a5df2acc1fee4ad3babdcdd0d4ce0552f71bcb2f
Nicolas Bouliane <contact@nicolasbouliane.com>
2022-07-01 12:03:40 +0200

    Added project aliases

--- dotfiles/aliases.sh
+++ dotfiles/aliases.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 alias dcmp=docker-compose
+
+alias touchbar="sudo killall TouchBarServer"
+
+alias aab="project AllAboutBerlin"
+alias hs="project HomeServer"
+alias tl="project Timeline"
